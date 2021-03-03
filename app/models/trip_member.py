f9a5fe0d5a9acbc18322753f5bc55e1727647f96
GaronSmith <wgaronsmith13@gmail.com>
2021-03-03 17:21:56 -0500

    created trip member model

---
+++ app/models/trip_member.py
@@ -0,0 +1,20 @@
+from .db import db
+
+class TripMember(db.Model):
+    __tablename__ = "trip_members"
+
+    id = db.Column(db.Integer, primary_key=True)
+    trip_id = db.Column(db.Integer, nullable=False, db.ForeignKey("trips.id"))
+    user_id = db.Column(db.Integer, nullable=False, db.ForeignKey("users.id"))
+    accepted = db.Column(db.Boolean, nullable=False, default=False)
+    leader = db.Column(db.Boolean, nullable=False, default=False)
+
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "trip_id": self.trip_id,
+            "user_id": self.user_id,
+            "accepted": self.accepted,
+            "leader": self.leader,
+        }
