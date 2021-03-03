735b7c8a70f818affb81cda0aaf9ed00e454a418
GaronSmith <wgaronsmith13@gmail.com>
2021-03-03 16:15:06 -0500

    created gear log

---
+++ app/models/gear_log.py
@@ -0,0 +1,27 @@
+from .db import db
+
+class GearLog(db.Model):
+    __tablename__ = 'gear_logs'
+
+    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    category_id = db.Column(db.Integer, nullable=False)
+    manufacturer = db.Column(db.String(200))
+    image_url = db.Column(db.Text)
+    status = db.Column(db.String(50))
+    purchase_date = db.Column(db.DateTime, nullable = False, default = datetime.now())
+    cost = db.Column(db.Integer)
+    quantity = db.Column(db.Integer, nullable=False, default = 1 )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "owner_id": self.owner_id,
+            "category_id": self.category_id,
+            "manufacturer": self.manufacturer,
+            "image_url": self.image_url,
+            "status": self.status,
+            "purchase_date": self.purchse_date,
+            "cost": self.cost,
+            "quantity": self.quantity,
+        }
