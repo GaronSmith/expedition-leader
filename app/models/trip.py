25d61baa57b3cacd51c8e3e0b9ba60ae46f35be6
GaronSmith <wgaronsmith13@gmail.com>
2021-03-03 17:05:46 -0500

    created trip model

---
+++ app/models/trip.py
@@ -0,0 +1,18 @@
+from .db import db
+from datetime import datetime
+
+class Trip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group_id = db.Column(db.Integer, db.ForeignKey("groups.id"), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    destination = db.Column(db.String(255), nullable=False)
+    date = db.Column (db.DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "group_id": self.group_id,
+            "name": self.name,
+            "destination": self.destination,
+            "date": self.date,
+        }
