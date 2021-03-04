3158ecd8cf5dfc02ed4c78c7a6ba458bb7ddf816
GaronSmith <wgaronsmith13@gmail.com>
2021-03-04 17:20:10 -0500

    stashing

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import {useSelector} from 'react-redux'
 import './NavBar.css'
 
 const NavBar = ({ setAuthenticated }) => {
+  const user = useSelector(state => state.session.user)
+  
   return (
-    <nav>
+    <nav className='navbar__container'>
+
       <ul>
         <li>
-          <NavLink to="/" exact={true} activeClassName="active">
+          <NavLink className="nav-link" to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
+          <NavLink className="nav-link" to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
         </li>
         <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
+          <NavLink className="nav-link" to="/sign-up" exact={true} activeClassName="active">
             Sign Up
           </NavLink>
         </li>
         <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
+          <NavLink className="nav-link" to="/users" exact={true} activeClassName="active">
             Users
           </NavLink>
         </li>
