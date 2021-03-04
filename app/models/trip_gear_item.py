5a277005808bb4dcd749049163bebf548b19ec53
GaronSmith <wgaronsmith13@gmail.com>
2021-03-04 15:28:05 -0500

    updated tripmember table

--- app/models/trip_gear_item.py
+++ app/models/trip_gear_item.py
@@ -1,6 +1,7 @@
 from .db import db
 
 trip_gear_items = db.Table(
-    trip_id=db.Column(db.Integer, db.ForeignKey('trips.id'), nullable=False, ),
-    gear_id=db.Column(db.Integer, db.ForeignKey("gear_logs.id"), nullable=False)
+    "trip_gear_items",
+    db.Column("trip_id", db.Integer, db.ForeignKey('trips.id'), primary_key=True, nullable=False, ),
+    db.Column("gear_id", db.Integer, db.ForeignKey("gear_logs.id"), primary_key=True, nullable=False)
 )
