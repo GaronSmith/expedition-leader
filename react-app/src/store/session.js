968183e8afdb06808fd624660d20aa87f165f0d3
GaronSmith <wgaronsmith13@gmail.com>
2021-03-04 15:54:11 -0500

    created session store

---
+++ react-app/src/store/session.js
@@ -0,0 +1,49 @@
+const SET_USER = "session/setUser"
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload:user
+    }
+}
+
+export const login = (email, password) => async (dispatch) =>{
+    const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    });
+    if(response.ok){
+        const user = await response.json()
+        dispatch(setUser(user))
+    }
+    return await response.json();
+}
+
+
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+
+    let newState;
+    switch (action.type) {
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload
+            return newState
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state
+    }
+}
+
+export default sessionReducer
