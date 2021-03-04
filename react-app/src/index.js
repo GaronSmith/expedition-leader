344ab559e1b2b6c9f7b82640436a1b741e139385
GaronSmith <wgaronsmith13@gmail.com>
2021-03-04 15:45:53 -0500

    updated index for redux

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
 import './index.css';
 import App from './App';
+import configureStore from "./store"
+
+if (process.env.NODE_ENV !== "production") {
+
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    </Provider>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
