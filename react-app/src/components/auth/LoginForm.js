26aafdb5b22ac1d9b15c625ae26df30fce911398
GaronSmith <wgaronsmith13@gmail.com>
2021-03-04 16:23:53 -0500

    updated auth

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { login } from "../../services/auth";
+import { setUser } from "../../store/session";
 
 const LoginForm = ({ authenticated, setAuthenticated }) => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch()
 
   const onLogin = async (e) => {
     e.preventDefault();
     const user = await login(email, password);
     if (!user.errors) {
       setAuthenticated(true);
+      dispatch(setUser(user))
     } else {
       setErrors(user.errors);
     }
