671620d6330255936126da494dd423c1f1ce4de0
GaronSmith <wgaronsmith13@gmail.com>
2021-03-06 15:35:02 -0500

    updated first sidebar item

--- react-app/src/components/DashBoard/SideBar/SideBar.js
+++ react-app/src/components/DashBoard/SideBar/SideBar.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBoxOpen } from '@fortawesome/free-solid-svg-icon
+import { faBoxOpen } from '@fortawesome/free-solid-svg-icon'
 import "./SideBar.css"
 
 const SideBar = () => {
 
     return (
         <div className="sidebar__container">
-
+            <ul>
+                <li>
+                    <Navlink >
+                        <FontAwesomeIcon icon={faBoxOpen} />
+                    </Navlink>
+                </li>
+            </ul>
         </div>
     )
 }
