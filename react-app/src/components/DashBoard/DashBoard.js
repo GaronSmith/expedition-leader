41e4a696ee3ad1a6c85d0a880741e182326ec71f
GaronSmith <wgaronsmith13@gmail.com>
2021-03-06 15:32:43 -0500

    installed font awesome

--- react-app/src/components/DashBoard/DashBoard.js
+++ react-app/src/components/DashBoard/DashBoard.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import "./DashBoard.css"
 
 const DashBoard = () => {
+    const user = useSelector(state => state.session.user)
+
     return (
+        <div className="dashboard__container">
+             
+        </div>
 
     )
 }
---
+++ react-app/src/components/DashBoard/SideBar/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBoxOpen } from '@fortawesome/free-solid-svg-icon
+import "./SideBar.css"
+
+const SideBar = () => {
+
+    return (
+        <div className="sidebar__container">
+
+        </div>
+    )
+}
+
+export default SideBar
