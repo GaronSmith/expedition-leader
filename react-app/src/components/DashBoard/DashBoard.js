685f2cfc4ce18b5bf14088b6b9a11000cebe8f02
GaronSmith <wgaronsmith13@gmail.com>
2021-03-07 19:30:53 -0500

    updated styling

--- react-app/src/components/DashBoard/DashBoard.js
+++ react-app/src/components/DashBoard/DashBoard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 import "./DashBoard.css"
+import GearBox from './GearBox'
 import SideBar from './SideBar'
 
 const DashBoard = () => {
@@ -9,7 +10,12 @@ const DashBoard = () => {
 
     return (
         <div className="dashboard__container">
-             <SideBar />
+            <div>
+                <SideBar />
+            </div>
+            <div className='gearbox'>
+                <GearBox />
+            </div>
         </div>
 
     )
--- react-app/src/components/DashBoard/GearBox/GearBox.js
+++ react-app/src/components/DashBoard/GearBox/GearBox.js
@@ -5,7 +5,7 @@ import './GearBox.css'
 const GearBox = () => {
     return (
         <div className='gearbox__container'>
-            
+            <h2>test</h2>
         </div>
     )
 }
