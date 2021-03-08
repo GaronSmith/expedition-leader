ee2d8d4e7ed5063e9ef16590f1a042bca216d3e1
GaronSmith <wgaronsmith13@gmail.com>
2021-03-08 10:43:28 -0500

    updated styling

--- react-app/src/components/DashBoard/GearBox/GearGroup/GearGroup.js
+++ react-app/src/components/DashBoard/GearBox/GearGroup/GearGroup.js
@@ -6,16 +6,17 @@ import './GearGroup.css'
 const GearGroup = ({group}) => {
 
     return (
-        <Collapsible trigger="Start here">
-            <p>
-                This is the collapsible content. It can be any element or React
-                component you like.
+            <Collapsible trigger="Start here">
+                <p>
+                    This is the collapsible content. It can be any element or React
+                    component you like.
             </p>
-            <p>
-                It can even be another Collapsible component. Check out the next
-                section!
+                <p>
+                    It can even be another Collapsible component. Check out the next
+                    section!
             </p>
-        </Collapsible>
+            </Collapsible>
+        
     )
 }
 
