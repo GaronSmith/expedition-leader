0c05c11f8349ec5267df3d0463b793611678950e
GaronSmith <wgaronsmith13@gmail.com>
2021-03-08 13:19:21 -0500

    created front - back thunk for gear categories

---
+++ app/api/gear_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import GearCategory
+
+gear_routes = Blueprint('gear', __name__)
+
+@gear_routes.route("/categories", methods=["GET"])
+def get_gear():
+    categories = GearCategory.query.all()
+    print('hit')
+    return {cat.id: cat.name for cat in categories}
+
+
--- react-app/src/components/DashBoard/GearBox/GearBox.js
+++ react-app/src/components/DashBoard/GearBox/GearBox.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import { getCategories } from '../../../store/gear'
 
 import './GearBox.css'
 import GearGroup from './GearGroup'
 
 const GearBox = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getCategories())
+    },[dispatch])
     return (
         <div className='gearbox__container'>
             <GearGroup />
