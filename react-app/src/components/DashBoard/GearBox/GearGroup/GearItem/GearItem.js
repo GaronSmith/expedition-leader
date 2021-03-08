0b6face80f9f2821cc15c917832d01f32e85dec4
GaronSmith <wgaronsmith13@gmail.com>
2021-03-08 17:25:14 -0500

    added item specs to gear item component

--- react-app/src/components/DashBoard/GearBox/GearGroup/GearItem/GearItem.js
+++ react-app/src/components/DashBoard/GearBox/GearGroup/GearItem/GearItem.js
@@ -8,7 +8,30 @@ const GearItem = ({item}) => {
     return (
         <div className='gear-item__container'>
             <Collapsible classParentString='item' trigger={`${item.name}`}>
-                <h1> hello</h1>
+                <div className='item__container'>
+                    <div className='item__left'>
+                        <img className='item__image' src={item.image_url} alt='img' />
+                    </div>
+                    <div className='item__right'>
+                        <ul className='item__list'>
+                            <li className='item__spec'>
+                                <span className='item__label'> Manufacturer:</span> {item.manufacturer ? item.manufacturer : "information not provided"}
+                            </li>
+                            <li className='item__spec'>
+                                <span className='item__label'> Cost per:</span> {item.cost ? `$${item.cost/100}` : "information not provided"}
+                            </li>
+                            <li className='item__spec'>
+                                <span className='item__label'> Quantity:</span> {item.quantity ? item.quantity : "information not provided"}
+                            </li>
+                            <li className='item__spec'>
+                                <span className='item__label'> Date acquired:</span> {item.purchase_date ? item.purchase_date.split(' ').slice(0, 4).join(' '): "information not provided"}
+                            </li>
+                            <li className='item__spec'>
+                                <span className='item__label'> Status:</span> {item.status ? item.status : "information not provided"}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </Collapsible>
         </div>
     )
