b35fa933246a626a78185cdbc2b473fd7270f5ed
GaronSmith <wgaronsmith13@gmail.com>
2021-03-09 06:18:56 -0500

    init

--- react-app/src/components/DashBoard/SideBar/SideBar.js
+++ react-app/src/components/DashBoard/SideBar/SideBar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBoxOpen } from '@fortawesome/free-solid-svg-icons'
 import GearBox from "../GearBox"
