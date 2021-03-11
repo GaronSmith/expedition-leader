4fe83ad378f6803b3ac7b83b6a6741fd815865e0
GaronSmith <wgaronsmith13@gmail.com>
2021-03-11 13:29:45 -0500

    created sidebarg group

--- react-app/src/components/GroupBoard/GroupBoard.js
+++ react-app/src/components/GroupBoard/GroupBoard.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import './GroupBoard.css'
 
 const GroupBoard = () => {
-
+    const user = useSelector(state => state.session.user)
+    const [view, setView] = useState("")
     return (
+        <div className="dashboard__container">
+            <div>
+                <SideBarGroup setView={setView} />
+            </div>
 
+            {view}
+        </div>
     )
 }
 
