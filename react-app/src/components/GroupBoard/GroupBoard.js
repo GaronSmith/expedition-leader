a80968a51214e929070733e2bf9f08839e79fe3f
GaronSmith <wgaronsmith13@gmail.com>
2021-03-11 14:17:41 -0500

    styling sidebar

--- react-app/src/components/GroupBoard/GroupBoard.js
+++ react-app/src/components/GroupBoard/GroupBoard.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useParams } from 'react-router'
 
 import './GroupBoard.css'
+import SideBarGroup from './SideBarGroup/'
 
 const GroupBoard = () => {
+    const groupId = useParams()
     const user = useSelector(state => state.session.user)
     const [view, setView] = useState("")
     return (
