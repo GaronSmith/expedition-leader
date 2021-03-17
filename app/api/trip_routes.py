6d833d40ec044f75a1f5ca3c5876f6946e9cc152
GaronSmith <wgaronsmith13@gmail.com>
2021-03-17 17:13:10 -0400

    built route for trips

---
+++ app/api/trip_routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+import json
+from sqlalchemy import and_
+from app.models import Trip, db
+
+trip_routes = Blueprint("trip", __name__)
+
+@trip_routes.route("/", methods = ['POST'])
+@login_required
+def get_group_trips():
+    data = json.loads(request.data)
+    group_id= data['groupId']
+
+    trips = Trip.query.filter(Trip.group_id == group_id).all()
+
+    return {trip.id: trip.to_dict() for trip in trips}
